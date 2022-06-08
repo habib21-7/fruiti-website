2468fb75f85d282882b296c6286b4ad09883d57b
Habeeb Alsheikh <habeebalsheikh8@gmail.com>
2022-06-09 02:12:55 +0300

    v2

--- fruiti/assets/js/form-validate.js
+++ fruiti/assets/js/form-validate.js
@@ -1,42 +1,32 @@
 function valid_datas( f ){
 	
-	if( f.name.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your name must not be empty!</span>');
-		notice( f.name );
-	}else if( f.email.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your email must not be empty and correct format!</span>');
-		notice( f.email );
-	//}else if( f.phone.value == '' ){
-		//jQuery('#form_status').html('<span class="wrong">Your phone must not be empty and correct format!</span>');
-		//notice( f.phone );
-	}else if( f.subject.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your subject must not be empty!</span>');
-		notice( f.subject );
-	}else if( f.message.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your message must not be empty!</span>');
-		notice( f.message );
+	if( f.username.value == '' ){
+		jQuery('#form_status').html('<span class="wrong">Your User Name Empty !</span>');
+		notice( f.username );
+	}else if( f.password.value == '' ){
+		jQuery('#form_status').html('<span class="wrong">Your Password empty !</span>');
+		notice( f.password );
 	}else{
 		 jQuery.ajax({
 			url: 'mail.php',
 			type: 'post',
-			data: jQuery('form#fruitkha-contact').serialize(),
+			data: jQuery('form#fruiti-contact').serialize(),
 			complete: function(data) {
 				jQuery('#form_status').html(data.responseText);
-				jQuery('#fruitkha-contact').find('input,textarea').attr({value:''});
-				jQuery('#fruitkha-contact').css({opacity:1});
-				jQuery('#fruitkha-contact').remove();
+				jQuery('#fruiti-contact').find('input').attr({value:''});
+				
 			}
 		});
 		jQuery('#form_status').html('<span class="loading">Sending your message...</span>');
-		jQuery('#fruitkha-contact').animate({opacity:0.3});
-		jQuery('#fruitkha-contact').find('input,textarea,button').css('border','none').attr({'disabled':''});
+		jQuery('#fruiti-contact').animate({opacity:0.3});
+		jQuery('#fruiti-contact').find('input,button').css('border','none').attr({'disabled':''});
 	}
 	
 	return false;
 }
 
 function notice( f ){
-	jQuery('#fruitkha-contact').find('input,textarea').css('border','none');
+	jQuery('#fruiti-contact').find('input').css('border','none');
 	f.style.border = '1px solid red';
 	f.focus();
 }
